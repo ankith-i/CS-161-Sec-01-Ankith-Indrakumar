6e039867998794ef63744c3bf0b99731374de6a9
ankith-i <61592964+ankith-i@users.noreply.github.com>
2024-05-10 01:16:27 -0700

    Update firebase-config.js

--- dev/src/firebase-config.js
+++ dev/src/firebase-config.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAdwNQEd8s6kvucvcHC6tnLCTE6JrqkaUg",
-    authDomain: "map-coloring.firebaseapp.com",
-    projectId: "map-coloring",
-    storageBucket: "map-coloring.appspot.com",
-    messagingSenderId: "934372614328",
-    appId: "1:934372614328:web:9dd3da12803e26fab2544d",
-    measurementId: "G-4HDV5L531V"
-  };
+  apiKey: "AIzaSyDYdkr_7LTiloMNfMLfv0FEsdFZ6Lviz08",
+  authDomain: "map-coloring-18ff5.firebaseapp.com",
+  projectId: "map-coloring-18ff5",
+  storageBucket: "map-coloring-18ff5.appspot.com",
+  messagingSenderId: "385764321127",
+  appId: "1:385764321127:web:5187a800917f208b6e71c2",
+  measurementId: "G-XCRS0VSRK3"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
